62767438a263dae22c6c3607858cff4963ec3a96
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-02 23:41:34 +0530

    Add files via upload

---
+++ Day03/code8.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void capitalize(string &str) {
+    str[0] = toupper(str[0]); 
+
+}
+
+int main() {
+    string subject = "computer science";
+    capitalize(subject);
+    cout << subject << endl;
+    return 0;
+}
