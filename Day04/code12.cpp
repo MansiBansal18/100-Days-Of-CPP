67c3d428f44e560d80918820f334425e6ccaf46c
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-03 23:38:26 +0530

    Add files via upload

---
+++ Day04/code12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void diamond(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) cout << " ";
+        for (int j = 0; j < 2 * i + 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) cout << " ";
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++) cout << "*";
+        cout << endl;
+    }
+}
+int main() {
+    int n;
+    n=5;
+    diamond(n);
+    return 0;
+}
