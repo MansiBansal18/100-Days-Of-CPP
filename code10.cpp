5d51c51420bf42d1405d878ac349f3f96c17a1d8
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-03 23:19:22 +0530

    Add files via upload

---
+++ code10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void Pyramid(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) cout << " ";
+        for (int j = 0; j < 2 * i + 1; j++) cout << "*";
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5; 
+    Pyramid(n); 
+    return 0;
+}
