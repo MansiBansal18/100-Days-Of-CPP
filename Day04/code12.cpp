d3a3827eee5633e0dac7f0223f24f876d0644df5
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-03 23:38:54 +0530

    Refactor diamond function and main in code12.cpp

--- Day04/code12.cpp
+++ Day04/code12.cpp
@@ -7,7 +7,6 @@ void diamond(int n) {
         for (int j = 0; j < 2 * i + 1; j++) cout << "*";
         cout << endl;
     }
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) cout << " ";
         for (int j = 0; j < 2 * n - (2 * i + 1); j++) cout << "*";
