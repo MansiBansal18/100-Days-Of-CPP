11ce9a62bd5d880d57f0444a9146597b851d42cb
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-04 20:20:08 +0530

    Fix formatting issues in code15.cpp

--- code15.cpp
+++ code15.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 void Tri(int n) {
     for (int i = 0; i < n; i++) {
-        // Inner loop runs from 1 to (n-i)
         for (int j = 1; j <= n - i; j++) {
             cout << j << " ";
         }
         cout << endl;
     }
 }
-
 int main() {
     int n;
     cout << "Enter size: ";
