a1e298c17b804c0bcc5ac2840e5fb609612c0984
MansiBansal18 <bansalmansi601@gmail.com>
2026-06-05 23:38:33 +0530

    Add files via upload

---
+++ Day06/code22.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void floydsTriangle(int n) {
+    int num = 1; 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++; 
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    floydsTriangle(n);
+    return 0;
+}
